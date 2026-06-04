cb4d4c229c7b840cd99c62640cf3aa8cf912b9b1
Vlad <vladradcuk5@gmail.com>
2026-06-04 17:32:27 +0300

    Delete books-app directory

--- books-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>Практична робота React + API</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- books-app/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-      <h1>Система керування книгами</h1>
-    </header>
-  );
-}
-
-export default Header;
